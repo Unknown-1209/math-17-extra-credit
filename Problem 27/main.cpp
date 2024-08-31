27f1d62e1709ea3a972e7cd3a5606d7542d1372d
Unknown1209 <110016688+Unknown-1209@users.noreply.github.com>
2024-08-31 00:13:30 -0700

    Update main.cpp

--- Problem 27/main.cpp
+++ Problem 27/main.cpp
@@ -15,11 +15,11 @@ int main() {
     double x0 = 0.0;  // Initial value of x
     double y0 = 0.0;  // Initial value of y 
     double deltaX = 0.01;
-    int steps = 100;   // Number of steps to approximate
+    int steps = 200;   // Number of steps to approximate
 
     double result = approx.eulerMethod(Function, x0, y0, deltaX, steps);
     
-    cout << "Using Euler's method to approximate the solution to dy/dx = x^2 + y^2\n";
+    cout << "Using Euler's method to approximate the solution to dy/dx = x^2 + y^2 - 1\n";
 
     cout << "Approximate value of y at x = " << approx.computeFinalX(x0, deltaX, steps) << " is " << result << endl;
 
